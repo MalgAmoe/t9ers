24fb98006c2d845632b65747a1beb2928e3c4df6
MalgAmoe <malgamoe@gmail.com>
2017-06-02 14:05:26 +0200

    feat: changed inputs and fields to use reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addNumber, eraseNumber } from './store/actions';
 import './App.css';
 
 const styles = {
@@ -15,19 +17,12 @@ const styles = {
 const acceptedInputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0'];
 
 class App extends Component {
-  state = {
-    text: ''
-  }
 
   inputHandler = (event) => {
     if (acceptedInputs.indexOf(event.key) !== -1) {
-      this.setState({
-        text: this.state.text + event.key
-      })
+      this.props.addNumber(event.key);
     } else if (event.key === 'Backspace') {
-      this.setState({
-        text: this.state.text.slice(0, this.state.text.length - 1)
-      })
+      this.props.eraseNumber()
     }
   }
 
@@ -47,7 +42,7 @@ class App extends Component {
         <div style={styles.numbers}>
           <input
             style={styles.input}
-            value={this.state.text}
+            value={this.props.numbers}
             placeholder='1234567890*'
             onKeyDown={this.inputHandler}/>
           <button onClick={this.handleSend}>Send</button>
@@ -57,4 +52,13 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  numbers: state.numbers
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  addNumber: (numbers) => dispatch(addNumber(numbers)),
+  eraseNumber: () => dispatch(eraseNumber())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
