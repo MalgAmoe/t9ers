165f5be38f7b5142a27c1f53c8a48834489d12f0
MalgAmoe <malgamoe@gmail.com>
2017-06-02 10:24:38 +0200

    feat: created transform number to string for getting possible words

--- server/controller/numberToString.js
+++ server/controller/numberToString.js
@@ -1,9 +1,11 @@
 // Function to transform the numbers from the keypad to possible words.
 // Arrays are used to scan the solutions from one type to the other.
 
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0'];
+const t = {};
 
-const characters = [
+t.numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0'];
+
+t.characters = [
   ['.', ',', '?', '!'],
   ['a', 'b', 'c'],
   ['d', 'e', 'f'],
@@ -17,17 +19,33 @@ const characters = [
   [' ']
 ];
 
-function getCharactersFromNumber(numberToCheck) {
-  return characters[numbers.indexOf(numberToCheck)]
+t.getCharactersFromNumber = function(numberToCheck) {
+  return t.characters[t.numbers.indexOf(numberToCheck)];
 }
 
+t.mixWordsAndCharacters = function(words, characters) {
+  if(words.length === 0) {
+    return characters;
+  }
+  let newWords = [];
+  let counter = 0;
+  words.forEach(word => {
+    characters.forEach(character => {
+      newWords[counter] = word.concat(character);
+      counter++
+    })
+  })
+  return newWords;
+}
 
+t.getPossibleWordsFromNumbers = function(numbersToTransform) {
+  let words = [];
+  numbersToTransform.forEach(number => {
+    if (t.numbers.indexOf(number) !== -1){
+      words = t.mixWordsAndCharacters(words, t.getCharactersFromNumber(number));
+    }
+  })
+  return words;
+}
 
-// export default function(numbersString) {
-//   const numbers = number.split('');
-//   const words = numbers.map(numbers => {
-//     let possibleWords =
-//   })
-// }
-
-console.log(getCharactersFromNumber('2'));
+module.exports = t;
